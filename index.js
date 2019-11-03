e2cacbf78dc3fef9bd955d761938b5579da9e37f
OKI Ayobami <46370698+xlassix@users.noreply.github.com>
2019-11-03 10:02:39 -0600

    comments

--- index.js
+++ index.js
@@ -2,24 +2,28 @@ const functions = require('firebase-functions');
 const express=require("express");
 const cors= require("cors")({options:true,credentials:true});
 const admin = require("firebase-admin");
-const uuid = require("uuid/v5"); //uuid for generating random id's for clients
+const uuid = require("uuid/v5");
 
 // initialize the database
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
   databaseURL: "https://reactxlassix.firebaseio.com",
 });
+
+//express app configuration with cors 
 const app = express()
 app.use(cors); //use cors for resource sharing
 app.use(express.urlencoded({extended:false})); //enable url-encoded types too
 app.use(express.json()); //enable us recieve json types in the request's body
 app.options('*', cors);
 
-    // this function cleans up the distorted json recieved
-    // function JSONize(str) {
-    //   return str.replace(/([\$\w]+)\s*:/g, function(_, $1){return '"'+$1+'":'}).replace(/'([^']+)'/g, function(_, $1){return '"'+$1+'"'})         
-    // }
+// this function cleans up the distorted json recieved
+// to be used when experimenting with postman or other request agents
+// function JSONize(str) {
+//   return str.replace(/([\$\w]+)\s*:/g, function(_, $1){return '"'+$1+'":'}).replace(/'([^']+)'/g, function(_, $1){return '"'+$1+'"'})         
+// }
   app.post('/', async (req,res)=>{
+    //set cross-orgin cors 
     res.set('Access-Control-Allow-Origin', '*');
     // Grab the text parameter.
     const original = JSON.parse(req.body);
@@ -27,10 +31,12 @@ app.options('*', cors);
     await admin.database().ref('/user_db').push({...original})
     return res.status(200).send(original)
   });
-    // add an evenlistener function to add an id to our user
+    
+
+//evenlistener function to add an userId to our user
 exports.addId = functions.database.ref('/user_db/{dataId}/')
     .onCreate((snapshot, context) => {
-        // get the unique pushId of this object in the db
+        //get pushId of this object in the db
         const dataId = context.params.dataId ;
         // use this Id with the uuid library to generate a unique ID
         const uniqueId= uuid(dataId,uuid.URL);
